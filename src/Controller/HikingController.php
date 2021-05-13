d39f86d0eb6a4ef660b1067f1ff5d39a832701de
Ludovic RAVINET <ludovic.ravinet@gmail.com>
2021-05-13 16:25:01 +0200

    code organisation

--- src/Controller/HikingController.php
+++ src/Controller/HikingController.php
@@ -14,22 +14,22 @@ class HikingController extends AbstractController
         return $this->twig->render('Hiking/show.html.twig', ['lands' => $lands, 'filter' => $filter]);
     }
 
-    public function landDisplay(int $X, int $Y)
+    // display description and image about a clicked balise land
+    public function landDisplay(int $coordX, int $coordY)
     {
         $filter = 2;
         $filter = $_SESSION['filter'];
         $hikingManager = new HikingManager();
         if ($filter == 2) {
-            $lands = $hikingManager->selectAll(2);
+            $lands = $hikingManager->selectAll();
         } elseif ($filter == 1) {
             $lands = $hikingManager->selectAllFilter(1);
         } else {
             $lands = $hikingManager->selectAllFilter(0);
         }
-        //$lands = $hikingManager->selectAll();
 
-        $X = (int)floor($X / 3);
-        $Y = (int)floor($Y / 3);
+        $coordX = (int)floor($coordX / 3);
+        $coordY = (int)floor($coordY / 3);
 
         $idPop = [];
         foreach ($lands as $land) {
@@ -37,7 +37,7 @@ class HikingController extends AbstractController
             $xMax = $land['coordinate_X'] + 20;
             $yMin = $land['coordinate_Y'] - 20;
             $yMax = $land['coordinate_Y'] + 20;
-            if ($X > $xMin && $X < $xMax && $Y > $yMin && $Y < $yMax) {
+            if ($coordX > $xMin && $coordX < $xMax && $coordY > $yMin && $coordY < $yMax) {
                 $idPop['id'] = $land['id'];
                 $idPop['name'] = $land['name'];
                 $idPop['description'] = $land['description'];
@@ -47,12 +47,16 @@ class HikingController extends AbstractController
 
         $choosenBalises = $_SESSION['baliseId'];
         $coordBalises = $_SESSION['coordBalises'];
-        //$coord = [(int)floor($X/3), (int)floor($Y/3)];
-        //var_dump($idPop);die;
 
-        return $this->twig->render('Hiking/show.html.twig', ['idPop' => $idPop, 'lands' => $lands, 'baliseId' => $choosenBalises, 'coordBalises' => $coordBalises, 'filter' => $filter]);
+        return $this->twig->render('Hiking/show.html.twig', ['idPop' => $idPop,
+            'lands' => $lands,
+            'baliseId' => $choosenBalises,
+            'coordBalises' => $coordBalises,
+            'filter' => $filter
+        ]);
     }
 
+    // display selected interest point balises
     public function baliseDisplay()
     {
         $choosenBalises = [];
@@ -62,26 +66,24 @@ class HikingController extends AbstractController
             $balisesId = array_map('trim', $_POST);
             $filter = $balisesId['filter'];
             $balisesId = array_slice($balisesId, 1);
-            //$balises = array_values($balises);
-            $hikingManager = new HikingManager();
 
+            $hikingManager = new HikingManager();
             if ($filter == 2) {
-                $balises = $hikingManager->selectAll(2);
+                $balises = $hikingManager->selectAll();
             } elseif ($filter == 1) {
                 $balises = $hikingManager->selectAllFilter(1);
             } else {
                 $balises = $hikingManager->selectAllFilter(0);
             }
-            //$balises = $hikingManager->selectAll();
+
             foreach ($balises as $bal) {
                 if (in_array($bal['id'], $balisesId)) {
                     $choosenBalises[] = (int)$bal['id'];
-                    $coordBalises .= (int)$bal['coordinate_X'] . '-' . (int)$bal['coordinate_Y'] . '-' . (int)$bal['id'] . ' ';
+                    $coordBalises .= (int)$bal['coordinate_X'] . '-' .
+                        (int)$bal['coordinate_Y'] . '-' . (int)$bal['id'] . ' ';
                 }
             }
             $coordBalises = substr($coordBalises, 0, -1);
-            //var_dump($coordBalises);die;
-            //var_dump($balisesId); var_dump($filter);die;
         }
 
         $_SESSION['baliseId'] = $choosenBalises;
@@ -90,13 +92,17 @@ class HikingController extends AbstractController
 
         $hikingManager = new HikingManager();
         if ($filter == 2) {
-            $lands = $hikingManager->selectAll(2);
+            $lands = $hikingManager->selectAll();
         } elseif ($filter == 1) {
             $lands = $hikingManager->selectAllFilter(1);
         } else {
             $lands = $hikingManager->selectAllFilter(0);
         }
-        //$lands = $hikingManager->selectAll();
-        return $this->twig->render('Hiking/show.html.twig', ['lands' => $lands, 'baliseId' => $choosenBalises, 'coordBalises' => $coordBalises, 'filter' => $filter]);
+
+        return $this->twig->render('Hiking/show.html.twig', ['lands' => $lands,
+            'baliseId' => $choosenBalises,
+            'coordBalises' => $coordBalises,
+            'filter' => $filter
+        ]);
     }
 }
